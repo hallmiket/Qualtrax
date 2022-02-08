023959085a83e51e8c37a474c2702abe2f329c90
hallmiket-admin <hall.miket@gmail.com>
2022-02-08 09:55:01 -0500

    V1

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            parseBracketString it = new parseBracketString();
+
+            bool a = it.parseTheString("(");
+            bool a1 = it.parseTheString("()[");
+            bool a2 = it.parseTheString("()");
+            bool b = it.parseTheString("()[]{}");
+            bool c = it.parseTheString("(]");
+            bool d = it.parseTheString("([)]");
+            bool e5 = it.parseTheString("{[]}");
+            bool f = it.parseTheString("{[()]}");
+            bool f2 = it.parseTheString("{[[{]]]}");
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MinStack minStack = new MinStack();
+            minStack.push(-2);
+            minStack.push(0);
+            minStack.push(-3);
+            minStack.getMin(); // return -3
+            minStack.pop();
+            minStack.top();    // return 0
+            minStack.getMin(); // return -2
+
+            minStack.push(-2147483648);
+            minStack.push(21474836471);
+        }
+    }
+}
---
+++ Questoions.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class MinStack
+    {
+        string[] stackCalls = new string[] { "MinStack", "push", "push", "push", "getMin", "pop", "top", "getMin" };
+
+        List<double> values;
+
+        public MinStack()
+        {
+            values = new List<double>();
+            return;
+        }
+
+        public void push(double valueToPush)
+        {
+            if (valueToPush >= Math.Pow(-2, 31) && valueToPush <= (Math.Pow(2, 31) - 1))
+            {
+                values.Add(valueToPush);
+            }
+            return;
+        }
+
+        public void pop()
+        {
+            values.RemoveAt(values.Count() - 1);
+            return;
+        }
+
+        public double top()
+        {
+            return values.FirstOrDefault();
+        }
+
+        public double getMin()
+        {
+            return values.Min();
+        }
+
+       
+    }
+
+    public class parseBracketString
+    {
+        public bool parseTheString(string s)
+        {
+            string sOpen1 = "(";
+            string sClose1 = ")";
+            string sOpen2 = "{";
+            string sClose2 = "}";
+            string sOpen3 = "[";
+            string sClose3 = "]";
+            bool returnThis1 = true;
+            bool returnThis2 = true;
+            bool returnThis3 = true;
+
+            if (s == null)
+            {
+                return false;
+            }
+            if (s.Length > Math.Pow(10, 4) || s.Length < 2)
+            {
+                return false;
+            }
+
+            //if the length of the string is odd, then it cannot be correct
+            if (s.Length % 2 != 0)
+            {
+                return false;
+            }
+            else
+            {
+                //check if brackets match: Handles Examples 1, 2, 3 & 4 in the Question doc
+                for (int i = 0; i <= s.Length - 2; i++)
+                {
+                    if (s[i].ToString() == sOpen1)
+                    {
+                        if (s[i + 1].ToString() != sClose1 && s[(s.Length - 1) - i].ToString() != sClose1)
+                        {
+                            returnThis1 = false;
+                        }
+                    }
+                    else if (s[i].ToString() == sOpen2 && s[i + 1].ToString() != sClose2)
+                    {
+                        if (s[i + 1].ToString() != sClose2 && s[(s.Length - 1) - i].ToString() != sClose2)
+                        {
+                            returnThis2 = false;
+                        }
+                    }
+                    else if (s[i].ToString() == sOpen3)
+                    {
+                        if (s[i + 1].ToString() != sClose3 && s[(s.Length - 1) - i].ToString() != sClose3)
+                        {
+                            returnThis3 = false;
+                        }
+                    }
+                }
+            }
+
+            return returnThis1 && returnThis2 && returnThis3;
+        }
+    }
+}
